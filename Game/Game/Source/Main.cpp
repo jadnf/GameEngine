fc7b4b771299e3114b87a209fed2d09b513a0a7a
J. Jadan fox <160644667+jadnf@users.noreply.github.com>
2024-06-28 10:13:04 -0600

    random line generator thingy

--- Game/Game/Source/Main.cpp
+++ Game/Game/Source/Main.cpp
@@ -1,9 +1,63 @@
 #include <iostream>
+#include <cstdlib>
 #include "../../Engine/Source/Test.h"
+#include <SDL.h>
 
 using namespace std;
 
-int main() {
-	cout << "hello world\n";
-	print();
+int main(int argc, char* argv[])
+{
+	// initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	// create window
+	// returns pointer to window if successful or nullptr if failed
+	SDL_Window* window = SDL_CreateWindow("Game Engine",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		800, 600,
+		SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	// create renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	while (true)
+	{
+		// clear screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+
+		// draw line
+		SDL_SetRenderDrawColor(renderer, 255, 25, 1, 0);
+		SDL_RenderDrawLine(renderer, 0, 0, 800, 600);
+		SDL_RenderDrawLine(renderer, 800, 600, 300, 300);
+		SDL_RenderDrawLine(renderer, 300, 300, 0, 0);
+		SDL_SetRenderDrawColor(renderer, 21, 123, 1, 0);
+		SDL_RenderDrawLine(renderer, 0, 400, 600, 400);
+		
+
+		
+		SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, rand() % 256);
+		SDL_RenderDrawLine(renderer, rand() % 801, rand() % 601, rand() % 801, rand() % 601);
+		SDL_RenderDrawLine(renderer, rand() % 801, rand() % 601, rand() % 801, rand() % 601);
+		SDL_RenderDrawLine(renderer, rand() % 801, rand() % 601, rand() % 801, rand() % 601);
+		SDL_RenderDrawLine(renderer, rand() % 801, rand() % 601, rand() % 801, rand() % 601);
+		SDL_RenderDrawLine(renderer, rand() % 801, rand() % 601, rand() % 801, rand() % 601);
+		
+
+
+		// show screen
+		SDL_RenderPresent(renderer);
+	}
+
+	return 0;
 }
